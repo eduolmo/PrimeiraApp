1667ce1781eefd7c3ae098124b4bc919d551817f
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-03-15 12:52:40 -0300

    Adicionei comentarios nos arquivos das telas

--- app/src/main/java/santana/olmo/primeiraapp/MainActivity.java
+++ app/src/main/java/santana/olmo/primeiraapp/MainActivity.java
@@ -16,15 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //pegando o botao na tela atraves do ID dele
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        //adicionando evento de click ao botao
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //criando o metodo que vai ser executado quando o botao for clicado
             public void onClick(View view) {
-
+                //pegando o campo de texto atraves do ID dele
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //pegando o texto dentro do campo de texto
                 String texto = etDigiteAqui.getText().toString();
+                //criando uma "intencao" de navegar para uma outra tela
                 Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                //inserindo na "intencao" uma informacao que deve ser trasmitida para a proxima tela
                 intent.putExtra("texto",texto);
+                //abrindo uma nova tela a partir da "intecao"
                 startActivity(intent);
             }
         });
--- app/src/main/java/santana/olmo/primeiraapp/NextActivity.java
+++ app/src/main/java/santana/olmo/primeiraapp/NextActivity.java
@@ -13,9 +13,13 @@ public class NextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
+        //pegando a "inteção" que foi utilizada para abrir essa tela
         Intent intent = getIntent();
+        //pegando a variavel que corresponde a 'texto' que foi trasmitida na "intencao"
         String texto = intent.getStringExtra("texto");
+        //pegando uma area de texto(diferente de campo de texto) da tela atraves do ID dela
         TextView tvExibir = findViewById(R.id.tvExibir);
+        //modificando o texto que esta dentro da area de texto
         tvExibir.setText(texto);
     }
 }
